9e4464f960f6b19b1e34c57b615e990e9147940a
gustavoLVJardim <utilizador@Mac-mini-de-utilizador.local>
2024-12-18 11:08:30 +0000

    new changes in css and js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const containerDivs = document.querySelector('.container');
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
+    
+
+for (let i = 0; i < 256; i++){
+    const createDivs = document.createElement('div');
+    createDivs.classList.add('divs');
+    containerDivs.appendChild(createDivs);
+}
+    
+    
     
 });
