dfa748b382c4c568b96e03629d49dccdd2e29179
gustavoLVJardim <utilizador@Mac-mini-de-utilizador.local>
2024-12-23 11:40:21 +0000

    new changes on html, css and js. Especialy on the js

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
 const containerDivs = document.querySelector('.container');
-
+const inputNumber = document.querySelector('#input-number');
+const submitBtn = document.querySelector('#submit-btn');    
 
 
 document.addEventListener('DOMContentLoaded', () => {
     
+    for (let i = 0; i < 16 * 16; i++){
+        const createDivs = document.createElement('div');
+        createDivs.style.width = 600 / 16 + 'px';
+        createDivs.style.height = 600 / 16 + 'px';
+        createDivs.classList.add('divs');
+        containerDivs.appendChild(createDivs);
+    }
 
-for (let i = 0; i < 256; i++){
-    const createDivs = document.createElement('div');
-    createDivs.classList.add('divs');
-    containerDivs.appendChild(createDivs);
-}
+    /*for (let i = 0; i < 256; i++){
+        const createDivs = document.createElement('div');
+        createDivs.classList.add('divs');
+        containerDivs.appendChild(createDivs);
+    }*/
+
+    submitBtn.addEventListener('click', () => {
+
+        if (inputNumber.value < 2 || inputNumber.value > 100) {
+            alert('Please enter a number between 2 and 100');
+        } else {
+            containerDivs.replaceChildren();
+            
+            for (let i = 0; i < inputNumber.value * inputNumber.value; i++){
+                const createDivs = document.createElement('div');
+                createDivs.style.width = 600 / inputNumber.value + 'px';
+                createDivs.style.height = 600 / inputNumber.value + 'px';
+                createDivs.classList.add('divs');
+                containerDivs.appendChild(createDivs);
+            }
+        }
+    }
     
     
     
+    );
 });
