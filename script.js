34c84c5b0ce3b8a5b732840651706d0f7c8e9004
gustavoLVJardim <utilizador@Mac-mini-de-utilizador.local>
2024-12-24 12:26:32 +0000

    more and more changes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const containerDivs = document.querySelector('.container');
 const inputNumber = document.querySelector('#input-number');
 const submitBtn = document.querySelector('#submit-btn');    
+const clearBtn = document.querySelector('#clear-btn');
+const radioButtons = document.querySelectorAll('input[type="radio"]');
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    
+function standardDivs() {
     for (let i = 0; i < 16 * 16; i++){
         const createDivs = document.createElement('div');
         createDivs.style.width = 600 / 16 + 'px';
@@ -12,31 +12,66 @@ document.addEventListener('DOMContentLoaded', () => {
         createDivs.classList.add('divs');
         containerDivs.appendChild(createDivs);
     }
+}
 
-    /*for (let i = 0; i < 256; i++){
+function createDivs(){
+    for (let i = 0; i < inputNumber.value * inputNumber.value; i++){
         const createDivs = document.createElement('div');
+        const containerSize = 600;
+        createDivs.style.width = containerSize / inputNumber.value + 'px';
+        createDivs.style.height = containerSize / inputNumber.value + 'px';
         createDivs.classList.add('divs');
         containerDivs.appendChild(createDivs);
-    }*/
+    }
 
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    standardDivs()
+    
     submitBtn.addEventListener('click', () => {
 
         if (inputNumber.value < 2 || inputNumber.value > 100) {
             alert('Please enter a number between 2 and 100');
         } else {
             containerDivs.replaceChildren();
-            
-            for (let i = 0; i < inputNumber.value * inputNumber.value; i++){
-                const createDivs = document.createElement('div');
-                createDivs.style.width = 600 / inputNumber.value + 'px';
-                createDivs.style.height = 600 / inputNumber.value + 'px';
-                createDivs.classList.add('divs');
-                containerDivs.appendChild(createDivs);
-            }
+            createDivs()
         }
     }
-    
-    
-    
     );
+
+    
+    clearBtn.addEventListener('click', () => {
+        containerDivs.replaceChildren();
+        createDivs()
+    }
+    );
+
+
+    radioButtons.forEach(radioButton => {
+        radioButton.addEventListener('click', () => {
+            if (radioButton.checked) {
+                const color = radioButton.id;
+                containerDivs.addEventListener('mouseover', () => {
+                    containerDivs.style.backgroundColor = color;
+                });
+            }
+        });
+    });
+    
+
+
+
+
+
+    
+
+
+
+
+
+
 });
